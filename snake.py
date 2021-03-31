569b7021e542fee26750f10dd29043e3f14f72c3
JoseRotsaert <jose.rotsaert@gmail.com>
2021-04-01 00:00:26 +0200

    updates

--- snake.py
+++ snake.py
@@ -1,10 +1,11 @@
 from turtle import Turtle
-STARTING_POSITIONS =[(0, 0), (-20, 0), (-40, 0)]
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
+
 
 class Snake:
 
@@ -15,11 +16,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.fillcolor("white")
-            new_segment.pu()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.fillcolor("white")
+        new_segment.pu()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):  # range(start=len(segments)-1; stop=0, step=-1):
@@ -43,3 +50,7 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+
+
+
+
